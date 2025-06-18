4c6d60aa7ae82bb217ac6871aa8974b919407f0c
robert thornton <robert.thornton92@gmail.com>
2025-06-18 16:02:13 +0300

    Added react-router with root and error components.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
+// Imported pages for the router.
+import Root from './routes/root';
+import ErrorPage from './components/Error/Error-page.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        errorElement: <ErrorPage />,
+        children: [
+          {
+            
+          },
+        ]
+      },
+
+    ]
+  },  
+]);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
