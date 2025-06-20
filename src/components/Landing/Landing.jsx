d06cc7d878a166a736d0d5ee88163bc938604976
robert thornton <robert.thornton92@gmail.com>
2025-06-20 10:26:19 +0300

    Added a skeleton landing page for development

---
+++ src/components/Landing/Landing.jsx
@@ -0,0 +1,12 @@
+
+
+export default function Landing() {
+  return (
+    <div className="landing">
+      <h1>Welcome to Our Store</h1>
+      <p>Discover a wide range of products tailored just for you.</p>
+      <p>Explore our categories and find what you love!</p>
+      {/* Add more content or components related to the landing page here */}
+    </div>
+  );
+}
