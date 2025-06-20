757a7904fb13316730441dda2747c88d310171ea
robert thornton <robert.thornton92@gmail.com>
2025-06-20 10:14:34 +0300

    Added paths and imported components for Landing page and a Categories page.

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,13 @@ import {
 // Imported pages for the router.
 import Root from './routes/root';
 import ErrorPage from './components/Error/Error-page.jsx';
+import Landing from './components/Landing/Landing.jsx';
+import Categories from './components/Categories/Categories.jsx';
 
+// import loaders and actions
+import CategoryPage, { loader as categoryPageLoader } from './components/Category/CategoryPage.jsx';
+
+// Create router adn paths.
 const router = createBrowserRouter([
   {
     path: "/",
@@ -20,8 +26,21 @@ const router = createBrowserRouter([
         errorElement: <ErrorPage />,
         children: [
           {
-            
+            index: true,
+            element: <Landing />,
+            // loader: landingLoader
           },
+          {
+            path: "Categories",
+            element: <Categories />,
+            // loader: categoriesLoader
+          },
+          {
+            path: "Categories/:category",
+            element: <CategoryPage />,
+            loader: categoryPageLoader
+          }
+
         ]
       },
 
