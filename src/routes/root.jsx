eb7127e6877044f1e9bc530d60cfd0fa00411919
robert thornton <robert.thornton92@gmail.com>
2025-06-20 10:27:49 +0300

    Added an Outlet to render child components

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,8 +1,15 @@
 
+import { Outlet } from 'react-router-dom';
 import Header from '../components/Header/Header.jsx';
+import Categories from '../components/Categories/Categories.jsx';
 
 export default function Root() {
   return (
-   <Header />
+    <>
+      <Header />
+      <div id="mainContent" className="contentContainer">
+        <Outlet />
+      </div>
+    </>
   );
 }
